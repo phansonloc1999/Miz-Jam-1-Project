2acdbab08da68912e1944452555630d96d2283c0
Lawless <vanthuan.cab93@gmail.com>
2020-08-23 12:05:44 +0700

    Update Menu

--- Assets/Scripts/Character/Master.cs
+++ Assets/Scripts/Character/Master.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Master : MonoBehaviour
 {
-    [SerializeField] private Map map;
+    private const float MAX_HEALTH = 3;
 
-    [SerializeField] private List<GameObject> summonedSlaves;
+    [SerializeField]
+    private float health = MAX_HEALTH;
 
-    [SerializeField] private List<GameObject> slavePrefabSet;
+    [SerializeField]
+    private Map map;
+
+    [SerializeField]
+    private List<GameObject> summonedSlaves;
+
+    [SerializeField]
+    private List<GameObject> slavePrefabSet;
 
     private void Awake()
     {
         map = GameObject.Find("Map").GetComponent<Map>();
+    }
+
+    public void takeDamage(float ammount)
+    {
+        health -= ammount;
     }
 
     public void moveToTile(GameObject newTile)
@@ -24,33 +37,9 @@ public class Master : MonoBehaviour
 
     public void summonSlaveAt(int slaveIndexInSet, int atMapRow, int atMapColumn)
     {
-        var newSlave = Instantiate(slavePrefabSet[slaveIndexInSet], Vector3.zero, transform.rotation, map.getTileAt(atMapRow, atMapColumn).transform);
-        newSlave.transform.up = gameObject.transform.up;
-        newSlave.transform.localPosition = new Vector3(0, 0, -0.5f);
-        newSlave.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        newSlave.GetComponent<Slave>().setMaster(this.gameObject);
-        newSlave.GetComponent<Health>().dead += onSlaveDeath;
-
+        var newSlave = Instantiate(slavePrefabSet[slaveIndexInSet], Vector3.zero, transform.rotation, map.getTile(atMapRow, atMapColumn).transform);
         summonedSlaves.Add(newSlave);
-    }
-
-    public delegate void SelectCharacterHandler(GameObject selectedChar);
-    public event SelectCharacterHandler selectedChar;
-    private void OnMouseOver()
-    {
-        if (Input.GetMouseButtonDown(MOUSE_BUTTON.PRIMARY))
-        {
-            selectedChar?.Invoke(this.gameObject);
-        }
-    }
-
-    public List<GameObject> getSummonedSlaves()
-    {
-        return summonedSlaves;
-    }
-
-    private void onSlaveDeath(GameObject deadSlave)
-    {
-        summonedSlaves.Remove(deadSlave);
+        newSlave.transform.localPosition = Vector3.zero;
+        newSlave.transform.localRotation = Quaternion.Euler(Vector3.zero);
     }
 }
