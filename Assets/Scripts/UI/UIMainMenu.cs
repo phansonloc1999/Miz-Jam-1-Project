bf71bb11e1a5dc8653e1f218d0aba4351efdbbf9
Lawless <vanthuan.cab93@gmail.com>
2020-08-24 23:11:22 +0700

    Add TextMeshPro Lib + New Font + Update Menu UI

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject _objNewGameMenu;
+    [SerializeField] GameObject _objOptionMenu;
+    [SerializeField] GameObject _objHelpMenu;
+    [SerializeField] GameObject _objExitMenu;
+
+    public void OnNewGameCLick()
+    {
+        gameObject.SetActive(false);
+
+        _objNewGameMenu.SetActive(true);
+        _objOptionMenu.SetActive(false);
+        _objHelpMenu.SetActive(false);
+    }
+
+    public void OnOptionClick()
+    {
+        gameObject.SetActive(false);
+
+        _objNewGameMenu.SetActive(false);
+        _objOptionMenu.SetActive(true);
+        _objHelpMenu.SetActive(false);
+        
+    }
+
+    public void OnHelpClick()
+    {
+        gameObject.SetActive(false);
+
+        _objNewGameMenu.SetActive(false);        
+        _objOptionMenu.SetActive(false);
+        _objHelpMenu.SetActive(true);
+    }
+
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
