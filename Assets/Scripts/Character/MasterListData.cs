e54ac37afa6645f3f373ad607699b438b5fac9b0
Lawless <vanthuan.cab93@gmail.com>
2020-08-26 16:13:23 +0700

    Create Master Data + Update New Game Main Menu

---
+++ Assets/Scripts/Character/MasterListData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ListMaster", menuName = "ListMasterData", order = 0)]
+public class MasterListData : ScriptableObject
+{
+    public List<MasterData> lstMasterData;
+}
+
+
--- Assets/Scripts/UI/UINewGame.cs
+++ Assets/Scripts/UI/UINewGame.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 namespace UI
 {
     enum Phase
@@ -11,34 +13,53 @@ namespace UI
     }
     public class UINewGame : MonoBehaviour
     {
-        [SerializeField] Image _labelObj;
+        [SerializeField] UIMainMenu _mainMenu;
+
+        [SerializeField] Image _imgLabel;
+        [SerializeField] Image _imgMaster;
+        [SerializeField] TextMeshProUGUI _tmpName;
         [SerializeField] GameObject _uiMapChoiceArrowLeft;
         [SerializeField] GameObject _uiMapChoiceArrowRight;
 
         [SerializeField] Sprite _labelPlayer1;
         [SerializeField] Sprite _labelPlayer2;
 
-        //[SerializeField] Sprite _playerIcon;
+        [SerializeField] MasterListData masterListData;
 
-        //Save Player Choice and Map
-
+        //For Choosing phase        
         private Phase _phase;
+        List<MasterData> _masterList = null;        
+        private int _currentIndex;
 
-        //private Dictionary<Master, bool> _masterList = null; Change to Master Info Later
-        
+        private MasterData _player1Choice;
+        private MasterData _player2Choice;
+        //TODO: private MapData 
 
-        private void OnEnable()
+        private void Awake()
         {
-            _phase = Phase.Player1Choice;
-            _labelObj.sprite = _labelPlayer1;
-            EnableUIArrow(true);
-            //load from Scripable Object
+            _masterList = new List<MasterData>();
+            
+            foreach(var masterData in masterListData.lstMasterData)
+            {
+                _masterList.Add(masterData);
+            }
+            ResetView();
         }
 
-        private void RefreshView()
+        private void OnDisable()
         {
-            //Change Sprite by choosen master
-            //Change text by choosen Master
+            ResetView();
+        }
+        #region private methods
+        private void ResetView()
+        {
+            _phase = Phase.Player1Choice;
+            _imgLabel.sprite = _labelPlayer1;
+            EnableUIArrow(true);
+
+            _player1Choice = _player2Choice = null;
+            _currentIndex = 0;
+            ChangeUI();
         }
 
         private void EnableUIArrow(bool isEnable)
@@ -47,21 +68,63 @@ namespace UI
             _uiMapChoiceArrowRight.SetActive(isEnable);
         }
 
+        private void SetNextMasterIndex()
+        {
+            int Count = _masterList.Count;
+            if(_currentIndex == Count - 1)
+            {
+                _currentIndex = 0;
+            }
+            else
+            {
+                _currentIndex++;
+            }
+        }
+
+        private void SetPrevMasterIndex()
+        {
+            int Count = _masterList.Count;
+            if (_currentIndex == 0)
+            {
+                _currentIndex = Count - 1;
+            }
+            else
+            {
+                _currentIndex--;
+            }
+        }
+
+        private void ChooseMaster(ref MasterData masterChoice)
+        {
+            masterChoice = _masterList[_currentIndex];
+            _masterList.Remove(masterChoice);
+            _currentIndex = 0;
+        }
+
+        private void ChangeUI()
+        {
+            _imgMaster.sprite = _masterList[_currentIndex].masterSprite;
+            _tmpName.text = _masterList[_currentIndex].masterName;
+        }
+        #endregion
+
+
+        #region public methods
         public void OnButtonDeclineClick()
         {
             switch (_phase)
             {
                 case Phase.Player1Choice:
-                    //back to main menu
+                    _mainMenu.RefreshView();
                     break;
                 case Phase.Player2Choice:
-                    //Change UI
-                    _labelObj.sprite = _labelPlayer1;
+                    _imgLabel.sprite = _labelPlayer1;
                     _phase = Phase.Player1Choice;
                     EnableUIArrow(true);
 
-                    //Submit Info
-                    //TODO: Add Choosen Master to List
+                    _masterList.Add(_player1Choice);
+                    _player1Choice = null;
+                    ChangeUI();
                     break;
             }            
         }
@@ -71,15 +134,19 @@ namespace UI
             switch (_phase)
             {
                 case Phase.Player1Choice:
-                    _labelObj.sprite = _labelPlayer2;
+                    _imgLabel.sprite = _labelPlayer2;
                     _phase = Phase.Player2Choice;
                     EnableUIArrow(false);
 
-                    //TODO: Remove Choosen Master from List
-
+                    ChooseMaster(ref _player1Choice);
+                    ChangeUI();
                     break;
                 case Phase.Player2Choice:
+                    ChooseMaster(ref _player2Choice);
+
                     //Init game
+                    Debug.Log(_player1Choice.masterName);
+                    Debug.Log(_player2Choice.masterName);
                     break;
             }
         }
@@ -96,12 +163,15 @@ namespace UI
 
         public void OnButtonNextMasterClick()
         {
-
+            SetNextMasterIndex();
+            ChangeUI();
         }
 
         public void OnButtonPrevMasterClick()
         {
-
+            SetPrevMasterIndex();
+            ChangeUI();
         }
+        #endregion
     }
 }
